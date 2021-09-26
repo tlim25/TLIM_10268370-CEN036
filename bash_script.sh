a2633dcf74f20684c49ae9abe9a1cc648004a1e3
Tais Lima <taisb_lima@usp.com.br>
2021-09-26 02:20:56 +0000

    adding bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# Comando para mostrar a pasta atual
 pwd
 
+# Comando para gerar lista com detalhes da pasta home e redirecioná-la para um arquivo
 
+ls -a /home/tlim > lista_HOME.txt 
